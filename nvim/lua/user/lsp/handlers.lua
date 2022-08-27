1521f6922ab7e610c47a547de7732f1514eeeeb3
Martin Landart <17906247+martinlandart@users.noreply.github.com>
2022-08-26 21:09:19 -0300

    toml support, better readme

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -97,6 +97,10 @@ M.on_attach = function(client, bufnr)
 		client.resolved_capabilities.document_formatting = false
 	end
 
+	if client.name == "taplo" then
+		client.resolved_capabilities.document_formatting = false
+	end
+
 	lsp_keymaps(bufnr)
 	local status_ok, illuminate = pcall(require, "illuminate")
 	if not status_ok then
