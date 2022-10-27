db1eb92e991baf0af119e5e8b01ef4aa1a0dc747
Martin Landart <17906247+martinlandart@users.noreply.github.com>
2022-10-26 23:19:50 -0300

    add background image to kitty, change theme to tokyonight-storm

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "catppuccin-macchiato"
+local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -95,6 +95,27 @@ return packer.startup(function(use)
 			})
 		end,
 	})
+	use("folke/tokyonight.nvim")
+	use({
+		"xiyaowong/nvim-transparent",
+		config = function()
+			require("transparent").setup({
+				enable = true, -- boolean: enable transparent
+				extra_groups = { -- table/string: additional groups that should be cleared
+					-- In particular, when you set it to 'all', that means all available groups
+
+					-- example of akinsho/nvim-bufferline.lua
+					"BufferLineTabClose",
+					"BufferlineBufferSelected",
+					"BufferLineFill",
+					"BufferLineBackground",
+					"BufferLineSeparator",
+					"BufferLineIndicatorSelected",
+				},
+				exclude = {}, -- table: groups you don't want to clear
+			})
+		end,
+	})
 	-- cmp plugins
 	use({ "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" }) -- The completion plugin
 	use({ "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" }) -- buffer completions
